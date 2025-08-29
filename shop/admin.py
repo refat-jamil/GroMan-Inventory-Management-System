08c6a7a1ef12fa8e365bb1bed6c7a506c3e66a95
Refat Jamil <s.m.refat.jamil@gmail.com>
2025-08-29 12:19:27 +0600

    MyUser admin registration in admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import MyUser
+
+
+@admin.register(MyUser)
+class MyUserAdmin(admin.ModelAdmin):
+    list_display = ("email", "first_name", "last_name", "is_admin", "is_active")
+    search_fields = ("email", "first_name", "last_name")
+    list_filter = ("is_admin", "is_active")
+    ordering = ("email",)
